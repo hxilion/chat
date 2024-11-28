a6d7c6835b713e4cb3dcd55b0d7032e5526bce2f
Lion <lion@luckylab.page>
2024-11-28 14:35:09 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { TUIKit } from "./TUIKit";
+import { TUICallKit } from "@tencentcloud/call-uikit-vue";
+import { TUITranslateService } from "@tencentcloud/chat-uikit-engine";
+import { TUIUserService } from "@tencentcloud/chat-uikit-engine";
+
+TUITranslateService.changeLanguage("zh");
+TUIUserService.switchMessageReadStatus(true); // 开启
+</script>
+
+<template>
+  <div>
+    <TUIKit
+      :SDKAppID="20014661"
+      userID="lion"
+      userSig="eJyrVgrxCdYrSy1SslIy0jNQ0gHzM1NS80oy0zLBwjmZ*XlQ8eKU7MSCgswUJSsjAwNDEzMzQ4h4akVBZlGqkpWhqakpUMYAIlqSmQsSMzc2MjI1Nje1hJqRmQ401N2tNL0ytTwqKlHbIMTbPUbfx8-c281Eu8C5uNJC2zzVz6*oyNUzwN88pCjZVqkWAMHSMFY_"
+    />
+    <TUICallKit
+      class="callkit-container"
+      :allowedMinimized="true"
+      :allowedFullScreen="false"
+    />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
